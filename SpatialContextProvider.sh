e3b957c90f40d46b1765de3749f2ce968f6abd14
Marek Rychlý <marek.rychly@gmail.com>
2011-04-13 13:48:27 +0000

    Update of shell scripts to Maven2 directory structure.

--- SpatialContextProvider.sh
+++ SpatialContextProvider.sh
@@ -2,9 +2,9 @@
 
 CLASS=$(basename $0 .sh)
 DIR=$(dirname $0)
-CLASSDIR=${DIR}/../../build/classes
-LIBDIR=${DIR}/../../lib/ws4d-jmeds
-LIBS=${LIBDIR}/ws4d-java-se-full.jar
+CLASSDIR=${DIR}/target/classes
+MAVENREPODIR=~/.m2/repository
+LIBS=${MAVENREPODIR}/org/ws4d/java/ws4d-java-se-full/2.0.0-beta4a/ws4d-java-se-full-2.0.0-beta4a.jar
 PACKAGE=eu.esonia.but.geoloc4d
 
 java -classpath "${CLASSDIR}:${LIBS}" "${PACKAGE}.${CLASS}" $*
