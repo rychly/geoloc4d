6ef06d40255b37154bf5c94450362c38898fd398
Marek Rychlý <marek.rychly@gmail.com>
2011-04-22 10:34:03 +0000

    TrilaterationStrategy interface methods and skeletons of its implementations.

--- src/main/java/eu/esonia/but/geoloc4d/util/StrategyWithRSSI.java
+++ src/main/java/eu/esonia/but/geoloc4d/util/StrategyWithRSSI.java
@@ -1,9 +1,36 @@
 package eu.esonia.but.geoloc4d.util;
 
+import eu.esonia.but.geoloc4d.type.MapOfNodes;
+import eu.esonia.but.geoloc4d.type.NodeData;
+import eu.esonia.but.geoloc4d.type.Vector3D;
+
 /**
  * The algorithm for selection of neighbouring nodes and computation of their distanecs from RSSI.
  * @author rychly
  */
 public class StrategyWithRSSI implements TrilaterationStrategy {
+
+    @Override
+    public TrilaterationStrategy makeTrilaterationStrategy() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void calibrateMetric(final NodeData node, final MapOfNodes neighbours)
+            throws TrilaterationStrategyException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public MapOfNodes selectNodesForTrilateration(final NodeData node, final MapOfNodes neighbours)
+            throws TrilaterationStrategyException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Vector3D doTrilateration(final MapOfNodes selectedNodes)
+            throws TrilaterationStrategyException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
 }
--- src/main/java/eu/esonia/but/geoloc4d/util/TrilaterationStrategy.java
+++ src/main/java/eu/esonia/but/geoloc4d/util/TrilaterationStrategy.java
@@ -1,5 +1,9 @@
 package eu.esonia.but.geoloc4d.util;
 
+import eu.esonia.but.geoloc4d.type.MapOfNodes;
+import eu.esonia.but.geoloc4d.type.NodeData;
+import eu.esonia.but.geoloc4d.type.Vector3D;
+
 /**
  * Interface of algorithms for selection of neighbouring nodes and computation of their distanecs for trilateration.
  * Strategy design pattern.
@@ -7,4 +11,39 @@ package eu.esonia.but.geoloc4d.util;
  */
 public interface TrilaterationStrategy {
 
+    /**
+     * Make a new instance of a specific trilateration strategy.
+     * Factory design pattern.
+     * @return the trilateration strategy
+     */
+    public TrilaterationStrategy makeTrilaterationStrategy();
+
+    /**
+     * Calibrate metric used by the trilateration strategy (e.g. transformation of RSSI/RTT to distance) for a reference node and its neighbours.
+     * @param node the reference node
+     * @param neighbours the neighbours of the reference node
+     * @throws TrilaterationStrategyException in case of incomplete data for the calibration
+     */
+    public void calibrateMetric(final NodeData node, final MapOfNodes neighbours)
+            throws TrilaterationStrategyException;
+
+    /**
+     * Select a group of nodes suitable for the trilateration strategy for a reference node and its neighbours.
+     * There can be different approaches for RSSI/RTT/distance based strategies.
+     * @param node the reference node
+     * @param neighbours the neighbours of the reference node
+     * @return the group of selected nodes (as a map)
+     * @throws TrilaterationStrategyException in case of incomplete data for the further trilateration
+     */
+    public MapOfNodes selectNodesForTrilateration(final NodeData node, final MapOfNodes neighbours)
+            throws TrilaterationStrategyException;
+
+    /**
+     * Perform the trilateration according to the specific strategy and selected nodes.
+     * @param selectedNodes node used for the trilateration
+     * @return result of the trilateration, i.e. location of blind node
+     * @throws TrilaterationStrategyException in case of incomplete data for the trilateration
+     */
+    public Vector3D doTrilateration(final MapOfNodes selectedNodes)
+            throws TrilaterationStrategyException;
 }
